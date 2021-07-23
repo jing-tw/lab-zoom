7806f7b60a18e007152a0995bbb227a90dd60f4a
Jing <mqjing@gmail.com>
2021-07-23 21:50:50 +0800

    [add] jing modify

--- lab/windows/test-0330/zoom-c-sharp-wrapper-5.5.12509.0330/zoom_sdk_demo/MainWindow.xaml.cs
+++ lab/windows/test-0330/zoom-c-sharp-wrapper-5.5.12509.0330/zoom_sdk_demo/MainWindow.xaml.cs
@@ -15,6 +15,13 @@ using System.Windows.Shapes;
 using System.ComponentModel; // CancelEventArgs
 using ZOOM_SDK_DOTNET_WRAP;
 
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+
+using ZoomIntegration.Configuration;
+
 namespace zoom_sdk_demo
 {
     /// <summary>
@@ -57,13 +64,87 @@ namespace zoom_sdk_demo
             //
             ZOOM_SDK_DOTNET_WRAP.AuthContext param = new ZOOM_SDK_DOTNET_WRAP.AuthContext();
             param.jwt_token = textBox_apptoken.Text;
+
+            Console.WriteLine("Zoom copyright!");
+            Console.WriteLine("generate websdk token!");
+
+            /*
+            string apiKey = "STxJ01JLrdJfX2FEag7jYfyt3iGtJWELKyB7";
+            string apiSecret = "WeAnA3XqRTC56p7Jhrby8PQF9sMCqokCl0cd";
+            string meetingNumber = "860 437 6369";
+            String ts = (ToTimestamp(DateTime.UtcNow.ToUniversalTime()) - 30000).ToString();
+            string role = "0";
+            string token = GenerateToken(apiKey, apiSecret, meetingNumber, ts, role);
+            Console.WriteLine(token);
+
+            param.jwt_token = token;
+            */
+
+
+            string apiKey = "YOUR_SDK_KEY";
+            string apiSecret = "YOUR_SDK_SECRET";
+            ZoomToken myToken = new ZoomToken(apiKey, apiSecret);
+            param.jwt_token = myToken.Token;
+            
+
+            /*
+            string apiKey = "STxJ01JLrdJfX2FEag7jYfyt3iGtJWELKyB7";
+            string apiSecret = "WeAnA3XqRTC56p7Jhrby8PQF9sMCqokCl0cd";
+            var tokenHandler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
+            var now = DateTime.UtcNow;
+            // var apiSecret = "<your API secret here>";
+            byte[] symmetricKey = Encoding.ASCII.GetBytes(apiSecret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = apiKey,
+                Expires = now.AddSeconds(30),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(symmetricKey), SecurityAlgorithms.HmacSha256),
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            var tokenString = tokenHandler.WriteToken(token);
+            Console.WriteLine("tokenString = " + tokenString);
+            param.jwt_token = tokenString;
+            */
+
             ZOOM_SDK_DOTNET_WRAP.CZoomSDKeDotNetWrap.Instance.GetAuthServiceWrap().SDKAuth(param);
             Hide();
         }
+
+        
 
         void Wnd_Closing(object sender, CancelEventArgs e)
         {
             System.Windows.Application.Current.Shutdown();
         }
+
+        static readonly char[] padding = { '=' };
+
+        public static long ToTimestamp(DateTime value)
+        {
+            long epoch = (value.Ticks - 621355968000000000) / 10000;
+            return epoch;
+        }
+
+        public static string GenerateToken(string apiKey, string apiSecret, string meetingNumber, string ts, string role)
+        {
+            string message = String.Format("{0}{1}{2}{3}", apiKey, meetingNumber, ts, role);
+            apiSecret = apiSecret ?? "";
+            var encoding = new System.Text.ASCIIEncoding();
+            byte[] keyByte = encoding.GetBytes(apiSecret);
+            byte[] messageBytesTest = encoding.GetBytes(message);
+            string msgHashPreHmac = System.Convert.ToBase64String(messageBytesTest);
+            byte[] messageBytes = encoding.GetBytes(msgHashPreHmac);
+            using (var hmacsha256 = new HMACSHA256(keyByte))
+            {
+                byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
+                string msgHash = System.Convert.ToBase64String(hashmessage);
+                string token = String.Format("{0}.{1}.{2}.{3}.{4}", apiKey, meetingNumber, ts, role, msgHash);
+                var tokenBytes = System.Text.Encoding.UTF8.GetBytes(token);
+                return System.Convert.ToBase64String(tokenBytes).TrimEnd(padding);
+            }
+        }
     }
 }
+
